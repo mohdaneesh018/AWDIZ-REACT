f555ec5cee1055d5fdfdd10371f1a37cf6ead51e
mohdaneesh018 <mdaneesh537@gmail.com>
2025-08-06 23:54:06 +0530

    Updates

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,66 @@
-import React from 'react' 
+import React from 'react'
 
 const Login = () => {
+    const [userData, setUserData] = React.useState({})
+
+    const [handlePassword, setHandlePassword] = React.useState(false);
+    const ShowPassword = () => {
+        setHandlePassword(!handlePassword);
+    }
+
+    const handleChange = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        });
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        try {
+            if (userData.email && userData.password) {
+                // api call
+                alert("Login successful!");
+                setUserData({
+                    email: "",
+                    password: ""
+                });
+            } else {
+                alert("Please fill all fields correctly.");
+            }
+        } catch (error) {
+            alert(error);
+        }
+    }
+
     return (
-        <>
-            Login
-        </>
+        <div>
+            <h1>Sign In</h1>
+            <h2>Email : {userData.email} Password : {userData.password}</h2>
+            <form>
+                <label>Email</label>
+                <br />
+                <input
+                    type="email"
+                    name="email"
+                    value={userData.email || ""}
+                    onChange={handleChange}
+                /><br />
+                <label>Password</label>
+                <br />
+                <input
+                    type={
+                        handlePassword ? "text" : "password"
+                    } name="password"
+                    value={userData.password || ""}
+                    onChange={handleChange} 
+                />
+                <button
+                    type='button'
+                    onClick={ShowPassword}>show</button>
+                <br />
+                <button type="submit" onClick={handleSubmit}>Login</button>
+            </form>
+        </div>
     );
 }
 
